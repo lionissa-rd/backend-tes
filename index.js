2ee5312cb37a54cfbc4b49ba91a0bd12b26185a7
lionissa-rd <lionissa.darmawan@gmail.com>
2019-09-18 13:51:59 +0700

    Adding JWT

--- index.js
+++ index.js
@@ -15,75 +15,75 @@ app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, and Postgres API'})
 })
 
-// COURSE
-app.get('/course/data', db.getCourse)
-app.post('/course/data', db.getCourseById)
-app.post('/course/create', db.createCourse)
-app.put('/course/update/:id', db.updateCourse)
-app.delete('/course/delete/:id', db.deleteCourse)
+// // COURSE
+// app.get('/course/data', db.getCourse)
+// app.post('/course/data', db.getCourseById)
+// app.post('/course/create', db.createCourse)
+// app.put('/course/update/:id', db.updateCourse)
+// app.delete('/course/delete/:id', db.deleteCourse)
 
-// COURSE_FILE
-app.get('/coursefile/data', db.getCourseFile)
-app.post('/coursefile/data', db.getCourseFileById)
-app.post('/coursefile/create', db.createCourseFile)
-app.put('/coursefile/update/:id', db.updateCourseFile)
-app.delete('/coursefile/delete/:id', db.deleteCourseFile)
+// // COURSE_FILE
+// app.get('/coursefile/data', db.getCourseFile)
+// app.post('/coursefile/data', db.getCourseFileById)
+// app.post('/coursefile/create', db.createCourseFile)
+// app.put('/coursefile/update/:id', db.updateCourseFile)
+// app.delete('/coursefile/delete/:id', db.deleteCourseFile)
 
-// COURSE_LEVEL
-app.get('/courselevel/data', db.getCourseLevel)
-app.post('/courselevel/data', db.getCourseLevelById)
-app.post('/courselevel/create', db.createCourseLevel)
-app.put('/courselevel/update/:id', db.updateCourseLevel)
-app.delete('/courselevel/delete/:id', db.deleteCourseLevel)
+// // COURSE_LEVEL
+// app.get('/courselevel/data', db.getCourseLevel)
+// app.post('/courselevel/data', db.getCourseLevelById)
+// app.post('/courselevel/create', db.createCourseLevel)
+// app.put('/courselevel/update/:id', db.updateCourseLevel)
+// app.delete('/courselevel/delete/:id', db.deleteCourseLevel)
 
-// COURSE_VIDEO
-app.get('/coursevideo/data', db.getCourseVideo)
-app.post('/coursevideo/data', db.getCourseVideoById)
-app.post('/coursevideo/create', db.createCourseVideo)
-app.put('/coursevideo/update/:id', db.updateCourseVideo)
-app.delete('/coursevideo/delete/:id', db.deleteCourseVideo)
+// // COURSE_VIDEO
+// app.get('/coursevideo/data', db.getCourseVideo)
+// app.post('/coursevideo/data', db.getCourseVideoById)
+// app.post('/coursevideo/create', db.createCourseVideo)
+// app.put('/coursevideo/update/:id', db.updateCourseVideo)
+// app.delete('/coursevideo/delete/:id', db.deleteCourseVideo)
 
-// EVENT
-app.get('/event/data', db.getEvent)
-app.post('/event/data', db.getEventById)
-app.post('/event/create', db.createEvent)
-app.put('/event/update/:id', db.updateEvent)
-app.delete('/event/delete/:id', db.deleteEvent)
+// // EVENT
+// app.get('/event/data', db.getEvent)
+// app.post('/event/data', db.getEventById)
+// app.post('/event/create', db.createEvent)
+// app.put('/event/update/:id', db.updateEvent)
+// app.delete('/event/delete/:id', db.deleteEvent)
 
-// EVENT_TYPE
-app.get('/eventtype/data', db.getEventType)
-app.post('/eventtype/data', db.getEventTypeById)
-app.post('/eventtype/create', db.createEventType)
-app.put('/eventtype/update/:id', db.updateEventType)
-app.delete('/eventtype/delete/:id', db.deleteEventType)
+// // EVENT_TYPE
+// app.get('/eventtype/data', db.getEventType)
+// app.post('/eventtype/data', db.getEventTypeById)
+// app.post('/eventtype/create', db.createEventType)
+// app.put('/eventtype/update/:id', db.updateEventType)
+// app.delete('/eventtype/delete/:id', db.deleteEventType)
 
-// INBOX
-app.get('/inbox/data', db.getInbox)
-app.post('/inbox/data', db.getInboxById)
-app.post('/inbox/create', db.createInbox)
-app.put('/inbox/update/:id', db.updateInbox)
-app.delete('/inbox/delete/:id', db.deleteInbox)
+// // INBOX
+// app.get('/inbox/data', db.getInbox)
+// app.post('/inbox/data', db.getInboxById)
+// app.post('/inbox/create', db.createInbox)
+// app.put('/inbox/update/:id', db.updateInbox)
+// app.delete('/inbox/delete/:id', db.deleteInbox)
 
-// NEWS
-app.get('/news/data', db.getNews)
-app.post('/news/data', db.getNewsById)
-app.post('/news/create', db.createNews)
-app.put('/news/update/:id', db.updateNews)
-app.delete('/news/delete/:id', db.deleteNews)
+// // NEWS
+// app.get('/news/data', db.getNews)
+// app.post('/news/data', db.getNewsById)
+// app.post('/news/create', db.createNews)
+// app.put('/news/update/:id', db.updateNews)
+// app.delete('/news/delete/:id', db.deleteNews)
 
-// NEWS_CATEGORY
-app.get('/newscategory/data', db.getNewsCategory)
-app.post('/newscategory/data', db.getNewsCategoryById)
-app.post('/newscategory/create', db.createNewsCategory)
-app.put('/newscategory/update/:id', db.updateNewsCategory)
-app.delete('/newscategory/delete/:id', db.deleteNewsCategory)
+// // NEWS_CATEGORY
+// app.get('/newscategory/data', db.getNewsCategory)
+// app.post('/newscategory/data', db.getNewsCategoryById)
+// app.post('/newscategory/create', db.createNewsCategory)
+// app.put('/newscategory/update/:id', db.updateNewsCategory)
+// app.delete('/newscategory/delete/:id', db.deleteNewsCategory)
 
-// NEWS_COMMENT
-app.get('/newscomment/data', db.getNewsComment)
-app.post('/newscomment/data', db.getNewsCommentById)
-app.post('/newscomment/create', db.createNewsComment)
-app.put('/newscomment/update/:id', db.updateNewsComment)
-app.delete('/newscomment/delete:id', db.deleteNewsComment)
+// // NEWS_COMMENT
+// app.get('/newscomment/data', db.getNewsComment)
+// app.post('/newscomment/data', db.getNewsCommentById)
+// app.post('/newscomment/create', db.createNewsComment)
+// app.put('/newscomment/update/:id', db.updateNewsComment)
+// app.delete('/newscomment/delete:id', db.deleteNewsComment)
 
 // TICKET
 app.get('/ticket/data', db.getTicket)
@@ -99,19 +99,19 @@ app.post('/ticketclass/create', db.createTicketClass)
 app.put('/ticketclass/update/:id', db.updateTicketClass)
 app.delete('/ticketclass/delete/:id', db.deleteTicketClass)
 
-//TICKET_USER
-app.get('/ticketuser/data', db.getTicketUser)
-app.post('/ticketuser/data', db.getTicketUserById)
-app.post('/ticketuser/create', db.createTicketUser)
-app.put('/ticketuser/update/:id', db.updateTicketUser)
-app.delete('/ticketuser/delete/:id', db.deleteTicketUser)
+// //TICKET_USER
+// app.get('/ticketuser/data', db.getTicketUser)
+// app.post('/ticketuser/data', db.getTicketUserById)
+// app.post('/ticketuser/create', db.createTicketUser)
+// app.put('/ticketuser/update/:id', db.updateTicketUser)
+// app.delete('/ticketuser/delete/:id', db.deleteTicketUser)
 
-// USER_BUSINESS
-app.get('/userbusiness/data', db.getUserBusiness)
-app.post('/userbusiness/data', db.getUserBusinessById)
-app.post('/userbusiness/create', db.createUserBusiness)
-app.put('/userbusiness/update/:id', db.updateUserBusiness)
-app.delete('/userbusiness/delete/:id', db.deleteUserBusiness)
+// // USER_BUSINESS
+// app.get('/userbusiness/data', db.getUserBusiness)
+// app.post('/userbusiness/data', db.getUserBusinessById)
+// app.post('/userbusiness/create', db.createUserBusiness)
+// app.put('/userbusiness/update/:id', db.updateUserBusiness)
+// app.delete('/userbusiness/delete/:id', db.deleteUserBusiness)
 
 // USER_LEVEL
 app.get('/userlevel/data', db.getUserLevel)
@@ -128,7 +128,13 @@ app.put('/users/update/:id', db.updateUsers)
 app.delete('/users/delete/:id', db.deleteUsers)
 
 // LOGIN
-app.post('/login', db.checkUser)
+app.post('/login', (req, res) => {
+  res.status(200).send({ access_token: '' });
+})
+
+app.post('/register', (req, res) => {
+  res.status(200).send({ access_token: '' });
+})
 
 app.listen(port, () => {
     console.log(
