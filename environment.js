fa60a3aab9846b5ebe412c4525b3e69d7d08c552
lionissa-rd <lionissa.darmawan@gmail.com>
2019-10-01 16:03:54 +0700

    fix

---
+++ environment.js
@@ -0,0 +1,8 @@
+module.exports = {
+    user: 'znzlkwbarqxmos',
+    host: 'ec2-54-243-243-76.compute-1.amazonaws.com',
+    database: 'dfe6td2dpbumh4',
+    password: 'af356e2a40618caee183fca772a997cf9392882c2ed9d161162ae7d4bf893d17',
+    port: 5432,
+    ssl: true
+};
